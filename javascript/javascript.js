2eae615a325861327fb69fdb494aec4e1403f208
FrancescoGalanti <galanti_francesco>
2020-10-29 14:19:38 +0100

    generazione cinque numeri fatta

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -10,12 +10,32 @@
  $(document).ready(function(){
   // funzione sincrona //
   setTimeout(myfunction, 3000);
-  console.log("ciao");
+  // test//
+
+  // console.log("ciao")
+
+ var listaRandomNumber = [];
+ var contatore = 5;
+
+ while(listaRandomNumber.length < contatore){
+      var number = Math.floor(Math.random() * 20 ) + 1;
+
+      if(! listaRandomNumber.includes(number)){
+        listaRandomNumber.push(number);
+      }
+
+      alert(number);
+ }
+
+ console.log(listaRandomNumber)
+
+
 
   // intervallo di 30 secondi //
 
   // funzione asincrona //
   function myfunction() {
+    // test//
    console.log("ciao dopo 3 secondi");
   }
 
